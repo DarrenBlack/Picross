fc4d5a69af90551c21b5ac01b03a47cf0641a88d
DESKTOP-P1SN4DF\Aaron <whiteon01@gmail.com>
2018-02-17 00:44:48 +0000

    Upload to Github

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockScript : MonoBehaviour {
+
+    Renderer rend;
+    public bool partOfPuzzle, correct;
+    BoardScript board;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+        board = GetComponentInParent<BoardScript>();
+
+
+        if (!partOfPuzzle) {
+            correct = true;
+        }
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButton(0)) {
+            Debug.Log(this.gameObject.name + " left click");
+            rend.material.color = Color.green;
+            if (partOfPuzzle) {
+                correct = true;
+            }
+            else {
+                correct = false;
+            }
+            board.CheckIfCorrect();
+        }
+        if (Input.GetMouseButton(1)) {
+            Debug.Log(this.gameObject.name + " right click");
+            rend.material.color = Color.red;
+            if (!partOfPuzzle) {
+                correct = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardScript : MonoBehaviour {
+    
+    public GameObject[] blocks = new GameObject[25];
+    bool complete;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetPuzzle(bool[] puzzleSolution) {
+        for(int i = 0; i < puzzleSolution.Length; i++) {
+            blocks[i].GetComponent<BlockScript>().partOfPuzzle = puzzleSolution[i];
+        }
+    }
+
+    public void CheckIfCorrect() {
+        int numberCorrect = 0;
+        for(int i = 0; i < blocks.Length; i++) {           
+
+            if (blocks[i].GetComponent<BlockScript>().correct) {
+                numberCorrect++;
+            }
+        }
+
+        if (numberCorrect == blocks.Length) {
+            Debug.Log("Puzzle Complete");
+        }
+    }
+}
